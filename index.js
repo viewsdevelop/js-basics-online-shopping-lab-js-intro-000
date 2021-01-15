44c3a668de2b2ba5ed99551673282e32f5da41ab
Louis Michael Medina <viewsdevelop@gmail.com>
2021-01-15 19:11:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ function total() {
 
 function removeFromCart(item) {
   if (cart.includes(item)) {
-    // delete cart[cart.indexOf(item)];
+    delete cart[cart.indexOf(item)];
     return "cart";
   } else {
     return "That item is not in your cart.";
